73ad380c5488da2746ddec49657218e2b64dddba
LuisEUM <luiseum95@gmail.com>
2022-06-02 04:24:23 +0200

    Cabezaso del lado derecho

--- js/constants.js
+++ js/constants.js
@@ -3,6 +3,8 @@ const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
 const KEY_LEFT = 37;
 const KEY_SPACE = 32;
+const KEY_CTRL = 17;
+
 
 const FPS = 60;
 const EARTH = 70;
--- js/player.js
+++ js/player.js
@@ -17,14 +17,19 @@ class Player {
     this.gravity = GRAVITY;
 
     this.characterImg = new Image();
-    this.characterImg.frames = 6;
     this.characterImg.frameIndex = 0;
     this.characterImg.src = '/img/Gousty_Sprite.png'
     this.shadowballs = []
+    this.framesCharacter = 6
+    this.characterImg.frames = 6;
 
     // hacer saber al juego a donde mira el personaje principal
     this.characterIsLookingRigth = true;
     this.characterIsLookingLeft = false;
+    this.audioShadowball = new Audio("/audio/Dark energy blast sound effect.mp3");
+    this.audioShadowball.volume = 0.01;
+    this.audioShadowball.loop = true;
+    this.audioShadowball.currentTime = 1;
   }
 
   draw() {
@@ -32,7 +37,7 @@ class Player {
       this.characterImg,
       (this.characterImg.width * this.characterImg.frameIndex) / this.characterImg.frames , 
       0, 
-      this.characterImg.width / 6, 
+      this.characterImg.width / this.characterImg.frames, 
       this.characterImg.height,
       this.x,
       this.y,
@@ -141,24 +146,31 @@ if (PlayerIsLookingLeft) {
       if (this.characterIsLookingRigth){
         this.characterImg.src = '/img/Gousty Loading - Loading Shadow Ball (2).png'
         console.log(this.tock)
+
+        this.audioShadowball.play()
       }
       if (this.characterIsLookingLeft){
         this.characterImg.src = '/img/Gousty_Loading_Loading_Shadow Ball_LEFT.png'
         console.log(this.tock)
-      }
 
+        this.audioShadowball.play()
+      }
       if (this.tock >= 30 && this.characterIsLookingRigth){
         this.characterIsLookingLeft = false
         this.shoot(this.characterIsLookingRigth, this.characterIsLookingLeft);
         this.tock = 15; 
-        this.characterImg.src = previousImgLookingSide 
+        this.characterImg.src = previousImgLookingSide
+
+        this.audioShadowball.currentTime = 2;
     }
 
-    if (this.tock >= 30 && this.characterIsLookingLeft){
+    if (this.tock >= 28 && this.characterIsLookingLeft){
       this.characterIsLookingRigth = false
       this.shoot(this.characterIsLookingRigth, this.characterIsLookingLeft);
-      this.tock = 15; 
+      this.tock = 14; 
       this.characterImg.src = previousImgLookingSide 
+
+      this.audioShadowball.currentTime = 2;
   }
     }
 
@@ -166,7 +178,9 @@ if (PlayerIsLookingLeft) {
     if (key === KEY_UP && this.vy === 0) {
       // TODO: jump and play jump sound
       this.vy = -10;
-      new Audio('/audio/Jump sound effect _ No copyright (192kbit_AAC).mp3').play();
+      const jumpAudio = new Audio('/audio/Jump sound effect _ No copyright (192kbit_AAC).mp3');
+      jumpAudio.volume = 0.01;
+      jumpAudio.play()
     }
 
     if (key === KEY_RIGHT) {
@@ -184,6 +198,15 @@ if (PlayerIsLookingLeft) {
     }
 
 
+    if (key === KEY_CTRL) {
+      this.vx += 1
+      this.characterImg.frames = 3;
+      this.characterImg.src = '/img/Gousty - Head Atack Fast.png'
+      if(this.vx === 2){
+        this.vx = 0
+      }
+    }
+
 
   }
 
@@ -191,19 +214,35 @@ if (PlayerIsLookingLeft) {
     if (key === KEY_RIGHT || key === KEY_LEFT) {
       this.vx = 0
     }
+
     if (key === KEY_RIGHT) {
       this.characterImg.src = '/img/Gousty_Sprite.png'
     }
+
     if (key === KEY_LEFT) {
       this.characterImg.src = '/img/Gousty_Sprite_Left.png'
     }
+
     if (key === KEY_SPACE && this.characterIsLookingRigth) {
       this.tock = 0
       this.characterImg.src = '/img/Gousty_Sprite.png'
+      this.audioShadowball.pause()
+      this.audioShadowball.currentTime = 0
     }
+
     if (key === KEY_SPACE && this.characterIsLookingLeft) {
       this.tock = 0
       this.characterImg.src = '/img/Gousty_Sprite_Left.png'
+      this.audioShadowball.pause()
+      this.audioShadowball.currentTime = 0
+    }
+
+    if (key === KEY_CTRL) {
+      this.characterImg.src = '/img/Gousty_Sprite.png'
+      this.vx = 0
+      this.characterImg.frames = 6
+
     }
   }
+  
 }
